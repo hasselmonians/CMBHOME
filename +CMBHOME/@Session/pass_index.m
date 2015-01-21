6839efc80579d3d2bf0ac7909d6141fcd7edf69e
wchapman <g.william.chapman.iv@gmail.com>
2015-01-21 16:21:11 -0500

    fixed plot_phase_precession

--- +CMBHOME/@Session/pass_index.m
+++ +CMBHOME/@Session/pass_index.m
@@ -7,7 +7,7 @@ if isempty(self.active_lfp)
     self.active_lfp = self.cel(1);
         varargin = varargin(2:end);    
 end
-results = CMBHOME.PASS_INDEX.pass_index(self.ts,[self.sx self.sy],self.spk.ts,self.lfp.ts,self.lfp.signal,varargin{:});
+results = CMBHOME.PASS_INDEX.pass_index('pos_ts',self.ts,'pos',[self.sx self.sy],'spk_ts',self.cel_ts{1},'lfp_ts',self.lfp.ts,'lfp_sig',self.lfp.signal,varargin{:});
 
 end
 
